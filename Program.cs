f13351cf1279ac6ce734b3a7920d58e7ce559b41
Mohamadmousavi1383 <148981551+Mohamadmousavi1383@users.noreply.github.com>
2023-11-01 02:35:58 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//mohammad mousavi fibonachy
+using Microsoft.VisualBasic;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int a = 0; int b = 1; int c;
+        Console.WriteLine("yak dune adad bego:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"{a}\t{b}\t");
+        for( int i = 2 ; i <= n ; i++ )
+        {
+            c = a + b;
+            Console.Write($"{c}\t");
+            a = b;
+            b = c;   
+            
+        }
+    }
+}
